3451a6c2f53256f8ce11734defc0d73057a9e80d
Varun G <vganni@cisco.com>
2020-10-17 11:45:09 +0530

    Initialize project using Create React App

---
+++ src/components/Cards.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class Cards extends Component{
+    render(){
+        return <div class="cards-container">Cards</div>
+    }
+}
+export default Cards
---
+++ src/components/Filters.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react';
+
+class Filters extends Component{
+    render(){
+        return <div class="filters-container">Filters</div>
+    }
+}
+export default Filters
